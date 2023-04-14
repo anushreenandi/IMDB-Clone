1abcbf2f93167d9175ea196b45fa85d91ea710d7
anushreenandi <110711445+anushreenandi@users.noreply.github.com>
2023-04-14 13:22:18 +0530

    first commit

---
+++ imbd-fe/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
+import MovieList from './Components/Movielist/Movielist';
+import MovieDetail from './pages/movieDetail/movie';
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<MovieDetail />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
